5267ab87fb76fe0cdee3802684d806b68a9fcfb7
Nasr-Edine DRAI <nasr-edine.drai@external.total.com>
2021-07-27 13:19:50 +0200

    remove old files

--- projects/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from projects.views import ProjectList, ProjectDetail
-
-urlpatterns = [
-    path('', ProjectList.as_view()),
-    path('<int:pk>/', ProjectDetail.as_view()),
-]
