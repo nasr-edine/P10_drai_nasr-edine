0f06d3b7403e5328a48b25ed3594d6bb8d2268e3
Nasr-Edine DRAI <nasr-edine.drai@external.total.com>
2021-08-05 09:30:24 +0200

    refactoring code

--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,5 @@
 # from rest_framework.exceptions import NotFound
+from django import http
 from rest_framework import generics
 
 from projects.serializers import ProjectSerializer
@@ -41,46 +42,26 @@ class RegisterAPI(generics.CreateAPIView):
 
 
 class ProjectList(generics.ListCreateAPIView):
-    """
-    List all projects for current user, or create a new project.
-    """
     permission_classes = [IsAuthenticated]
     serializer_class = ProjectSerializer
 
-    # Get list projects filtering by current user
     def get_queryset(self):
         queryset = Project.objects.filter(
             contributors=self.request.user)
         return queryset
 
-    # def list(self, request):
-    #     queryset = self.get_queryset()
-    #     if queryset:
-    #         serializer = ProjectSerializer(queryset, many=True)
-    #         return Response(serializer.data)
-    #     else:
-    #         raise NotFound()
-
 
 class ProjectDetail(generics.RetrieveUpdateDestroyAPIView):
-    """
-    Retrieve, update or delete a project instance.
-    """
     permission_classes = [IsAuthenticated, IsCreatorOrReadOnlyForContributor]
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
 
 
 class ProjectContributorsList2(generics.ListCreateAPIView):
-    """
-    List all contributors for a given project, or add a new user to a given project.
-    """
     permission_classes = [IsAuthenticated, IsCreatorOrReadOnlyForContributor]
     serializer_class = ContributorSerializer
 
-    # Get list contributors filtering by current user
     def get_queryset(self):
-        print('get_queryset called')
         project = Project.objects.get(pk=self.kwargs['pk'])
         queryset = Contributor.objects.filter(project=project)
         return queryset
@@ -93,7 +74,6 @@ class ProjectContributorsList2(generics.ListCreateAPIView):
         except User.DoesNotExist:
             return Response({"message": "We can't add this user because he doesn't exist in database."},
                             status=status.HTTP_404_NOT_FOUND)
-
         try:
             project = Project.objects.get(pk=self.kwargs['pk'])
         except Project.DoesNotExist:
@@ -111,130 +91,119 @@ class ProjectContributorsList2(generics.ListCreateAPIView):
 
 
 class ProjectContributorDestroy(generics.DestroyAPIView):
-    # queryset = ProjectContributor.object
-    permission_classes = [IsAuthenticated, IsCreatorOrReadOnlyForContributor]
 
     serializer_class = ContributorSerializer
-    # queryset = self.get_queryset()
     queryset = Contributor.objects.all()
-
-    # def get_queryset(self):
-    #     print('get_queryset for destroy called')
-    #     project = Project.objects.get(pk=self.kwargs['pk'])
-    #     print(project)
-    #     user = User.objects.get(pk=self.kwargs['pk_user'])
-    #     print(user)
-    #     contributor = Contributor.objects.filter(
-    #         project=project, user=user).distinct()
-    #     contributor = Contributor.objects.all()
-    #     print(contributor[0])
-
-    #     print(contributor)
-    #     return contributor
+    permission_classes = [IsAuthenticated, IsCreatorOrReadOnlyForContributor]
 
     def delete(self, request, pk, pk_user, format=None):
-        print('delete called')
-
-        project = Project.objects.get(pk=self.kwargs['pk'])
-        print(project)
-        user = User.objects.get(pk=self.kwargs['pk_user'])
-        print(user)
-        contributor = Contributor.objects.get(
-            project=project, user=user)
+        try:
+            project = Project.objects.get(pk=self.kwargs['pk'])
+        except Project.DoesNotExist:
+            raise Http404
+        try:
+            user = User.objects.get(pk=self.kwargs['pk_user'])
+        except User.DoesNotExist:
+            raise Http404
+        try:
+            contributor = Contributor.objects.get(
+                project=project, user=user)
+        except Contributor.DoesNotExist:
+            raise Http404
         if contributor.role == 'creator':
             return Response({'message': "Yon can remove yourself from the project because you are author"}, status=status.HTTP_403_FORBIDDEN)
         contributor.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-class ProjectContributorsList(APIView):
-    """
-    List all contributors for a given project, or add a new user to a given project.
-    """
-    queryset = Project.objects.all()
-    serializer_class = ProjectSerializer
-    permission_classes = [IsAuthenticated]
+# class ProjectContributorsList(APIView):
+#     """
+#     List all contributors for a given project, or add a new user to a given project.
+#     """
+#     queryset = Project.objects.all()
+#     serializer_class = ProjectSerializer
+#     permission_classes = [IsAuthenticated]
 
-    def get_object(self, pk):
-        try:
-            return Project.objects.get(pk=pk)
-        except Project.DoesNotExist:
-            raise Http404
+#     def get_object(self, pk):
+#         try:
+#             return Project.objects.get(pk=pk)
+#         except Project.DoesNotExist:
+#             raise Http404
 
-    def get(self, request, pk, format=None):
-        project = self.get_object(pk=pk)
-        contributors = project.contributors.all()
-        contributors2 = Contributor.objects.filter(project=project)
-        # contributors2.role = 'contributor'
-        serializer2 = ContributorSerializer(contributors2, many=True)
-        # serializer2.save()
-        print(contributors2)
-        if not contributors:
-            return Response({"message": "There are not contributors to this project."},
-                            status=status.HTTP_404_NOT_FOUND)
-        serializer = UserSerializer(contributors, many=True)
-        return Response({"Project name": project.title,
-                         "contributors": serializer2.data})
+#     def get(self, request, pk, format=None):
+#         project = self.get_object(pk=pk)
+#         contributors = project.contributors.all()
+#         contributors2 = Contributor.objects.filter(project=project)
+#         # contributors2.role = 'contributor'
+#         serializer2 = ContributorSerializer(contributors2, many=True)
+#         # serializer2.save()
+#         print(contributors2)
+#         if not contributors:
+#             return Response({"message": "There are not contributors to this project."},
+#                             status=status.HTTP_404_NOT_FOUND)
+#         serializer = UserSerializer(contributors, many=True)
+#         return Response({"Project name": project.title,
+#                          "contributors": serializer2.data})
 
-    def post(self, request, pk, format=None):
-        project = self.get_object(pk=pk)
-        try:
-            creator = User.objects.filter(
-                project=project, contributor__role='creator')
-        except User.DoesNotExist:
-            return Response({"message": "There are not creator for this project save in database."},
-                            status=status.HTTP_404_NOT_FOUND)
-        if request.user in creator:
-            serializer = AddUserSerializer(data=request.data)
-            if serializer.is_valid():
-                try:
-                    user = User.objects.get(
-                        username=serializer.data['username'])
-                except User.DoesNotExist:
-                    return Response({"message": "We can't add this user because he doesn't exist in database."},
-                                    status=status.HTTP_404_NOT_FOUND)
-                contributors = project.contributors.all()
-                if not user in contributors:
-                    project.contributors.add(user)
-                    contributor = Contributor.objects.get(
-                        project=project, user=user)
-                    contributor.role = 'contributor'
-                    contributor.save()
-                else:
-                    return Response({"message": "This user is already added to the project."},
-                                    status=status.HTTP_400_BAD_REQUEST)
-            else:
-                return Response(serializer.errors)
-            return Response({"Project name": project.title,
-                             "user added": UserSerializer(user).data})
-        else:
-            return Response({"message": "You are not allowed to add a user to this project. Only the creator can add a new user"},
-                            status=status.HTTP_403_FORBIDDEN)
+#     def post(self, request, pk, format=None):
+#         project = self.get_object(pk=pk)
+#         try:
+#             creator = User.objects.filter(
+#                 project=project, contributor__role='creator')
+#         except User.DoesNotExist:
+#             return Response({"message": "There are not creator for this project save in database."},
+#                             status=status.HTTP_404_NOT_FOUND)
+#         if request.user in creator:
+#             serializer = AddUserSerializer(data=request.data)
+#             if serializer.is_valid():
+#                 try:
+#                     user = User.objects.get(
+#                         username=serializer.data['username'])
+#                 except User.DoesNotExist:
+#                     return Response({"message": "We can't add this user because he doesn't exist in database."},
+#                                     status=status.HTTP_404_NOT_FOUND)
+#                 contributors = project.contributors.all()
+#                 if not user in contributors:
+#                     project.contributors.add(user)
+#                     contributor = Contributor.objects.get(
+#                         project=project, user=user)
+#                     contributor.role = 'contributor'
+#                     contributor.save()
+#                 else:
+#                     return Response({"message": "This user is already added to the project."},
+#                                     status=status.HTTP_400_BAD_REQUEST)
+#             else:
+#                 return Response(serializer.errors)
+#             return Response({"Project name": project.title,
+#                              "user added": UserSerializer(user).data})
+#         else:
+#             return Response({"message": "You are not allowed to add a user to this project. Only the creator can add a new user"},
+#                             status=status.HTTP_403_FORBIDDEN)
 
-    def delete(self, request, pk_project, pk_user, format=None):
-        project = self.get_object(pk=pk_project)
-        try:
-            creator = User.objects.filter(
-                project=project, contributor__role='creator')
-        except User.DoesNotExist:
-            return Response({"message": "There are not creator for this project save in database."},
-                            status=status.HTTP_404_NOT_FOUND)
-        if request.user in creator:
-            print(project)
-            try:
-                contributor = project.contributors.get(pk=pk_user)
-            except User.DoesNotExist:
-                return Response({"message": "This  user doesn't exist in this project."},
-                                status=status.HTTP_404_NOT_FOUND)
+#     def delete(self, request, pk_project, pk_user, format=None):
+#         project = self.get_object(pk=pk_project)
+#         try:
+#             creator = User.objects.filter(
+#                 project=project, contributor__role='creator')
+#         except User.DoesNotExist:
+#             return Response({"message": "There are not creator for this project save in database."},
+#                             status=status.HTTP_404_NOT_FOUND)
+#         if request.user in creator:
+#             print(project)
+#             try:
+#                 contributor = project.contributors.get(pk=pk_user)
+#             except User.DoesNotExist:
+#                 return Response({"message": "This  user doesn't exist in this project."},
+#                                 status=status.HTTP_404_NOT_FOUND)
 
-            try:
-                project.contributors.remove(contributor)
-            except ProtectedError:
-                return Response({"message": "This user can't be deleted!!"},
-                                status=status.HTTP_400_BAD_REQUEST)
+#             try:
+#                 project.contributors.remove(contributor)
+#             except ProtectedError:
+#                 return Response({"message": "This user can't be deleted!!"},
+#                                 status=status.HTTP_400_BAD_REQUEST)
 
-            return Response({"message": "User Removed Successfully from the project."},
-                            status=status.HTTP_204_NO_CONTENT)
-        else:
-            return Response({"message": "You are not allowed to remove a user from this project. Only the creator can remove a user"},
-                            status=status.HTTP_403_FORBIDDEN)
+#             return Response({"message": "User Removed Successfully from the project."},
+#                             status=status.HTTP_204_NO_CONTENT)
+#         else:
+#             return Response({"message": "You are not allowed to remove a user from this project. Only the creator can remove a user"},
+#                             status=status.HTTP_403_FORBIDDEN)
